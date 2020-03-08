0889b6e2cfb86080323f80623cb0e98b196ddc87
Lonny Gomes <code@lonnygomes.com>
2020-03-08 01:37:07 -0500

    chore: add haiku layout item
    refs #1

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('src/css');
-
+    eleventyConfig.addLayoutAlias('base', 'base.njk');
+    eleventyConfig.addLayoutAlias('haiku', 'haiku.njk');
     return {
         dir: {
             input: 'src'
