6de53eb932446033a8b1b049b9d7da5d03b5f372
Lonny Gomes <code@lonnygomes.com>
2020-03-08 17:42:25 -0400

    chore: add debug filter for testing
    refs #1

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,13 @@
+const util = require('util');
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('src/css');
+
     eleventyConfig.addLayoutAlias('base', 'base.njk');
     eleventyConfig.addLayoutAlias('haiku', 'haiku.njk');
+
+    eleventyConfig.addFilter('debug', data => util.inspect(data));
+
     return {
         dir: {
             input: 'src'
