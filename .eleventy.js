5ee1e1349e0ddb8876d549e1b9b41fdcf6c52346
Lonny Gomes <code@lonnygomes.com>
2020-03-09 01:45:46 -0400

    feat: add short code for images
    refs #7

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 const util = require('util');
 
 module.exports = function(eleventyConfig) {
+    const url = urlStr => eleventyConfig.nunjucksFilters.url(urlStr);
+
     eleventyConfig.addPassthroughCopy('src/css');
     eleventyConfig.addPassthroughCopy('src/assets');
 
@@ -8,6 +10,13 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addLayoutAlias('haiku', 'haiku.njk');
 
     eleventyConfig.addFilter('debug', data => util.inspect(data));
+    eleventyConfig.addShortcode(
+        'image',
+        (path, alt = '', width = '100%') =>
+            `<img class="img" src="/assets/images/${url(
+                path
+            )}" alt="${alt}" width="${width}">`
+    );
 
     return {
         dir: {
